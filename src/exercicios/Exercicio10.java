a59ed1283c06034ec9562b97cafcc215fad282d5
vanessaalmei <vanessalmeida.slv@gmail.com>
2021-09-11 22:59:27 -0300

    Exercicio 10

--- src/exercicios/Exercicio10.java
+++ src/exercicios/Exercicio10.java
@@ -7,16 +7,37 @@ import java.util.Scanner;
  * a fórmula abaixo
  * 
  * x',x" = -b+- raizquadrada b²-4*a*c dividido 2*a
- *
- * RETOMAR
- *
+ * 
+ * @see https://bhaskaracalculator.netlify.app/
+ * 
  */
 
 public class Exercicio10 {
 
 	public static void main(String[] args) {
 
-		// TODO: FAZER DEPOIS
+		Scanner leia = new Scanner(System.in);
+
+		System.out.print("Digite o valor A: ");
+		double valorA = leia.nextDouble();// 27
+
+		System.out.print("Digite o valor B: ");
+		double valorB = leia.nextDouble();// 48
+
+		System.out.print("Digite o valor C: ");
+		double valorC = leia.nextDouble();// 12
+
+	
+		double delta = Math.pow(valorB, 2) - 4 * valorA * valorC;
+
+		double x1 = -valorB + Math.sqrt(delta);
+		x1 = x1 / (2 * valorA);
+
+		double x2 = -valorB - Math.sqrt(delta);
+		x2 = x2 / (2 * valorA);
+
+		System.out.println("O valor de x1 é: " + x1);
+		System.out.println("O valor de x2 é: " + x2);
 
 	}
 
